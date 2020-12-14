04b5ad5413e8469d848421ee9594a6b7fffc1add
Jordi Cluet <45299503+Makanut009@users.noreply.github.com>
2020-12-14 12:30:00 +0100

    Line sound volume adjusted

--- Qomp3D/Qomp3D/Player.cpp
+++ Qomp3D/Qomp3D/Player.cpp
@@ -256,6 +256,7 @@ void Player::update(int deltaTime, vector<Wall*>* walls, vector<BallSpike*>* bal
 			if (lastVelocity == 0) {
 				lastVelocity = velocity.y;
 				line_channel = SoundManager::instance().playSound(line_sound);
+				line_channel->setVolume(2.0f);
 			}
 			velocity.y = 0;
 		}
@@ -263,6 +264,7 @@ void Player::update(int deltaTime, vector<Wall*>* walls, vector<BallSpike*>* bal
 			if (lastVelocity == 0) {
 				lastVelocity = velocity.x;
 				line_channel = SoundManager::instance().playSound(line_sound);
+				line_channel->setVolume(2.0f);
 			}
 			velocity.x = 0;
 		}
